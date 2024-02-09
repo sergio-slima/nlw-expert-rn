a06b6417f7187ac469db4438f43e4fff93a91963
sergio-slima <sergio_silvalima@hotmail.com>
2024-02-09 07:43:57 -0300

    add tailwind

---
+++ src/app/_layout.tsx
@@ -0,0 +1,11 @@
+import { SafeAreaView } from "react-native"
+import { Slot } from "expo-router"
+
+export default function Layout() {
+    return(
+        <SafeAreaView className="flex-1 bg-slate-900">
+            <Slot/>
+        </SafeAreaView>
+        
+    )
+}
--- src/app/index.tsx
+++ src/app/index.tsx
@@ -1,9 +1,15 @@
 import {View, Text} from "react-native"
+import {
+    Inter_400Regular,
+    Inter_500Medium,
+    Inter_600SemiBold,
+    Inter_700Bold,    
+} from "@expo-google-fonts/inter"
 
 export default function Home(){
     return (
-        <View>
-            <Text>Hello React Native!</Text>
+        <View className="flex-1 pt-8">
+            <Text className="text-white text-2xl">Hello React Native!</Text>
         </View>
     )
 }
