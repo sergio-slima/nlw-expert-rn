8dcf73d9823723735606b694c9548f028ad26c42
sergio-slima <sergio_silvalima@hotmail.com>
2024-02-10 23:45:11 -0300

    creats lists

--- src/components/header.tsx
+++ src/components/header.tsx
@@ -1,10 +1,13 @@
-import { Image, View, Text } from "react-native";
+import { Image, View, Text, TouchableOpacity } from "react-native";
+import { Feather } from "@expo/vector-icons"
+import colors from "tailwindcss/colors"
 
 type HeaderProps = {
     title: string
+    cartQuantityItems?: number
 }
 
-export function Header({ title }: HeaderProps) {
+export function Header({ title, cartQuantityItems = 0 }: HeaderProps) {
     return(
         <View className="flex-row items-center border-b border-slate-700 pb-5 mx-5">
             <View className="flex-1">
@@ -12,9 +15,14 @@ export function Header({ title }: HeaderProps) {
                 <Text className="text-white text-xl font-heading mt-2">{title}</Text>
             </View>
 
-            <View className="bg-lime-300 w-4 h-4 rounded-full items-center justify-center">
-                <Text className="text-slate-900 font-bold text-xs">1</Text>
-            </View>
+            {cartQuantityItems > 0 && (
+                <TouchableOpacity className="relative" activeOpacity={0.7}>
+                <View className="bg-lime-300 w-4 h-4 rounded-full items-center justify-center top-2 z-10 -right-3.5">
+                    <Text className="text-slate-900 font-bold text-xs">{cartQuantityItems}</Text>
+                </View>
+
+                <Feather name="shopping-bag" color={colors.white} size={24} />
+            </TouchableOpacity>)}
         </View>
     )
 }
